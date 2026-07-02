8e57c142ef5cbd60e1c717a475d183726d32fd2e
JYOTIRMOY DAS <dascse378@gmail.com>
2026-07-02 04:53:22 +0000

    init

---
+++ src/subarray2.java
@@ -0,0 +1,32 @@
+public class subarray2 {
+    public static void main(String[] args) {
+
+        int[] arr = {3, 5, 2, 1, 8, 4, 9};
+        int k = 3;
+
+        int overallMaxOdd = Integer.MIN_VALUE;
+
+        for (int i = 0; i <= arr.length - k; i++) {
+
+            int maxOdd = Integer.MIN_VALUE;
+
+            for (int j = i; j < i + k; j++) {
+
+                if (arr[j] % 2 != 0 && arr[j] > maxOdd) {
+                    maxOdd = arr[j];
+                }
+            }
+
+
+            if (maxOdd > overallMaxOdd) {
+                overallMaxOdd = maxOdd;
+            }
+        }
+
+        if (overallMaxOdd == Integer.MIN_VALUE) {
+            System.out.println("No odd number");
+        } else {
+            System.out.println("Maximum Odd = " + overallMaxOdd);
+        }
+    }
+}
