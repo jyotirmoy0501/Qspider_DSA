352720d98f8f54dbb369d82a17848a6968c4c9b0
Jyotirmoy DAS <145799966+dascse@users.noreply.github.com>
2026-07-04 19:54:22 +0530

    init

---
+++ src/NonDuplicate.java
@@ -0,0 +1,20 @@
+public class NonDuplicate {
+
+        public static int findSingle(int[] arr) {
+            int result = 0;
+
+            for (int num : arr) {
+                result = result ^ num;
+            }
+
+            return result;
+        }
+
+        public static void main(String[] args) {
+
+            int[] arr = {4, 1, 2, 1, 2};
+
+            System.out.println("Non-duplicate element = " + findSingle(arr));
+        }
+    }
+
