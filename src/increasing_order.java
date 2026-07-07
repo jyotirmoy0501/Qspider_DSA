16b0820351d97bef829d4c4ef408134f027ab874
Jyotirmoy DAS <145799966+dascse@users.noreply.github.com>
2026-07-07 12:02:15 +0530

    init

--- src/increasing_order.java
+++ src/increasing_order.java
@@ -1,2 +1,22 @@
 public class increasing_order {
+    public  static void main(String[] args){
+        int arr=12345;
+        int  d=10;
+        while(arr>=10){
+            int temp=arr%10;
+            if(d>temp){
+                d=temp;
+
+                System.out.println("T");
+
+            }
+            else{
+                System.out.println("F");
+                break;
+            }
+            arr=arr/10;
+        }
+
+
+    }
 }
