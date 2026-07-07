1666f2a8f1515d34ba339c59d57c76bd75c3c3a5
Jyotirmoy DAS <145799966+dascse@users.noreply.github.com>
2026-07-07 15:34:35 +0530

    init

---
+++ src/LinearSearch.java
@@ -0,0 +1,16 @@
+public class LinearSearch {
+    public static void main(String[] args) {
+        int[] num = {1, 2, 3, 5, 4};
+        int i;
+        int temp = 5;
+        for (i = 0; i < num.length - 1; i++) {
+            if (num[i] == temp)
+            {
+                System.out.println(+num[i]+" in index "+i);
+                //for(int j=0;j<num.length-1-i;j++){
+            }
+
+
+        }
+    }
+}
