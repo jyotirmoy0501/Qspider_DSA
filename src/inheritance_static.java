049a76cac6d42c7f3a7a4023b68168fd0920c689
Jyotirmoy DAS <145799966+dascse@users.noreply.github.com>
2026-07-09 14:22:17 +0530

    init

---
+++ src/inheritance_static.java
@@ -0,0 +1,8 @@
+public class inheritance_static {
+    static int a=10;
+}
+class  sample extends inheritance_static{
+    public static void main(String[] args){
+        System.out.println(a);
+    }
+}
