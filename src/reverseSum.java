065f41acebe124ee39c653b5d87b7fb08efbbcf1
Jyotirmoy DAS <145799966+dascse@users.noreply.github.com>
2026-07-15 11:01:56 +0530

    init

---
+++ src/reverseSum.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class reverseSum {
+
+    public static int sumOfPrime(int n) {
+
+        // Reverse the number
+        int rev = 0;
+        int temp = n;
+
+        while (temp > 0) {
+            rev = rev * 10 + temp % 10;
+            temp = temp / 10;
+        }
+
+        // Find the range
+        int start = Math.min(n, rev);
+        int end = Math.max(n, rev);
+
+        int sum = 0;
+
+        // Find prime numbers in the range
+        for (int i = start; i <= end; i++) {
+
+            if (i < 2)
+                continue;
+
+            boolean isPrime = true;
+
+            for (int j = 2; j * j <= i; j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime) {
+                sum += i;
+            }
+        }
+
+        return sum;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        System.out.println(sumOfPrime(n));
+
+        sc.close();
+    }
+}
